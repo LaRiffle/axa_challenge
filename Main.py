aba1c6273e702d93b6d4b8fa2dfcab8c2e7783f0
RobinCoste <robin.coste@polytechnique.edu>
2016-11-03 12:50:59 +0100

    split update

--- Main.py
+++ Main.py
@@ -14,7 +14,7 @@ fields = ['DATE', 'DAY_OFF', 'DAY_DS', 'WEEK_END', 'DAY_WE_DS','TPER_TEAM', 'ASS
 
 
 
-data=pd.read_csv("data/train_2011_2012_2013.csv", sep=";", usecols=fields, nrows=100) # LECTURE
+data=pd.read_csv("data/train_2011_2012_2013.csv", sep=";", usecols=fields, nrows=1000) # LECTURE
 
 x = data[fields[0:-2]] # Data sans les received calls
 y = data[fields[-1]] # label = received calls
@@ -26,7 +26,7 @@ y = y[ass=='Crises']
 
 
 x = preprocess(x) # rajoute les features
-xTrain, xTest, yTrain, yTest = faireSplitting(x) # rajoute les features
+xTrain, xTest, yTrain, yTest = faireSplitting(x, y) # rajoute les features
 #A_DEFINIR = train(xTrain, yTrain) # rajoute les features
 #yEmpirique = test(A_DEFINIR, xTest) # rajoute les features
 #check(yEmpirique, yTest)
--- fonction_py/split.py
+++ fonction_py/split.py
@@ -1,5 +1,5 @@
+import numpy as np
 
-
-def faireSplitting(x):
-    
-    return 0
+def faireSplitting(x, y):
+    ln = (np.random.rand(x.shape[0]) < 0.8)
+    return 0, 0, 0, 0;
