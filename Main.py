d75c9fcd327800691a43da9aa40af4251498f6ef
RobinCoste <robin.coste@polytechnique.edu>
2016-11-03 10:13:23 +0100

    ajoute des coms'

--- Main.py
+++ Main.py
@@ -20,14 +20,13 @@ import time
 start_time = time.time()
 print("go")
 fields = ['DATE', 'DAY_OFF', 'DAY_DS', 'WEEK_END', 'DAY_WE_DS','TPER_TEAM', 'ASS_ASSIGNMENT', 'CSPL_RECEIVED_CALLS' ]
-data=pd.read_csv("data/train_2011_2012_2013.csv", sep=";", usecols=fields, nrows=10)
+data=pd.read_csv("data/train_2011_2012_2013.csv", sep=";", usecols=fields, nrows=10) # LECTURE
 
-x = data[fields[0:-2]]
-y = data[fields[-1]]
-ass = data[fields[-2]]
-#x['DATE'] = pd.to_datetime(x['DATE'])
+x = data[fields[0:-2]] # Data sans les received calls
+y = data[fields[-1]] # label = received calls
+ass = data[fields[-2]] # ass assignment = differentes categories a predire
 
-x = pd.concat([x,x['DATE']], axis=1)
+x = pd.concat([x,x['DATE']], axis=1) # test
 print(x)
 
 print("--- %s seconds ---" % (time.time() - start_time))
