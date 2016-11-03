04094896e4c1650e6ced6d391713d5f319d6c5d7
Théo Leffyr <Tleffyr@MacBook-Pro-de-Theo.local>
2016-11-03 11:19:29 +0100

    Précie les étapes de preprocessing

--- fonction_py/preprocess.py
+++ fonction_py/preprocess.py
@@ -1,5 +1,8 @@
 
 
 def preprocess(x):
-
+    # Ajoute les champs utiles et supprime ceux qui servent à rien
+    # Transformation en champs numériques
+    # Normalise (mean 0, std 1)
+    # Ajoute des champs d'ordre deux pour une régression polynomiale
     return(x)
