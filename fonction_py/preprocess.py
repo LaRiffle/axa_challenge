e46cd3baae7be42613af34d08645d8a54b8389ad
Théo Leffyr <Tleffyr@MacBook-Pro-de-Theo.local>
2016-11-03 11:04:53 +0100

    Précise les tâches à accomplir dans preprocessing

--- fonction_py/preprocess.py
+++ fonction_py/preprocess.py
@@ -1,4 +1,8 @@
 
 
 def preprocess(x):
+    # Créé les champs utiles et supprime ceux qui sont non pertinents
+    # Modifie les champs en valeurs pertinentes numériques
+    # Normalise les champs numériques (mean = 0, std = 1)
+    # Créé les champs d'ordre 2 pour faire une régression polynomiale d'ordre 2
     return(x)
