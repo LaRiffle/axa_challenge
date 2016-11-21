9036851fa48cc2ebf865a8ce57994f1d44db57cf
PCTIMOTHE\Tim <timothe.mercat@gmail.com>
2016-11-21 15:01:22 +0100

    commit de mise à jour

--- fonction_py/tim.py
+++ fonction_py/tim.py
@@ -35,15 +35,15 @@ def linearLinexpMinimization(x, y):
     print("ok")
 
     print("AVEC")
-    pca = decomposition.PCA(n_components=65)#65)
+    pca = decomposition.PCA(n_components=65)
     pca.fit(xTrain)
     PCAxTrain = pca.transform(xTrain)
     nbLines,nbFeatures = PCAxTrain.shape
-    res = minimize(fun_to_min,np.zeros(nbFeatures+1),args=(PCAxTrain,yTrain.values))
+    res = minimize(fun_to_min,np.zeros(nbFeatures+1),args=(PCAxTrain,yTrain))
     PCAxTest = pca.transform(xTest)
-    x = res.x
-    a=x[:-1]
-    b=x[-1]
+    x = res.x #x est de longeur nbFeatures+1
+    a=x[:-1] #a.T est une colonne de longueur nbFeatures
+    b=x[-1] #b est un scalaire
     print("a : \n",a)
     print("b : \n",b)
     pred = np.dot(PCAxTest,np.transpose(a))+b
@@ -52,6 +52,19 @@ def linearLinexpMinimization(x, y):
     bins = np.linspace(-10, 10, 40)
     plt.hist(pred-yTest, bins, normed=1)
     
+def telephoniePred(x,y,xTest):
+    pca = decomposition.PCA(n_components=65)#65)
+    pca.fit(x)
+    PCAxTrain = pca.transform(x)
+    nbLines,nbFeatures = PCAxTrain.shape
+    res = minimize(fun_to_min,np.zeros(nbFeatures+1),args=(PCAxTrain,y))
+    PCAxTest = pca.transform(xTest)
+    x = res.x
+    a=x[:-1]
+    b=x[-1]
+    pred = np.dot(PCAxTest,np.transpose(a))+b
+    pred = np.round(pred)
+    return pred
     
     
 #    print("PCA")
@@ -90,3 +103,24 @@ def linearLinexpMinimization(x, y):
 #    del x['DATE']
 #
 #    x=pd.get_dummies(x)
+
+#a : 
+# [ -4.51187364e+00   3.38242455e+00  -1.21221693e+01   2.09881551e-02
+#  -4.41768572e-01   2.14374943e+00  -2.44087620e-01  -7.36365590e-01
+#   6.20131421e+00   3.97019347e+00   2.61540336e+00   5.07557175e+00
+#  -2.36070480e+00   7.83770186e-01  -2.85907549e+00   5.98147782e-01
+#  -3.71732393e+00   1.13077517e+00   4.20373369e+00   4.36745759e-01
+#  -2.68431728e+00  -4.43218981e-01   5.81247338e-01   6.17176255e-01
+#  -9.80813609e-01  -1.09948028e+00  -1.15989481e+00  -4.12295756e-01
+#  -2.66160200e-01   8.91540111e-02  -2.95623735e-01  -1.33402628e+00
+#   9.39698099e-01  -1.45401109e-01   5.44124934e-02   7.90849079e-01
+#   2.25779615e+00   4.16458818e-01   7.55368925e-01  -1.39918346e-01
+#  -2.60203698e-01  -2.70301801e-01   6.98076852e-01   4.32647696e-01
+#   5.83255563e-01  -3.72538379e-01  -1.04008611e+00   8.46517742e-01
+#   2.66915968e-01   6.04541556e-01   1.01594631e+01   6.73908394e+00
+#   7.47238332e+00   1.28802616e+01  -8.33981596e+00  -8.77078860e+00
+#   5.06659923e-01   5.82519167e+00  -2.19151804e+01  -4.65302708e+00
+#  -1.10262775e+01   1.10886327e+01   3.36040552e+01  -2.00647693e+01
+#   1.15964000e+01]
+#b : 
+# 13.4367650654
--- fonction_py/train.py
+++ fonction_py/train.py
@@ -1,5 +1,188 @@
-from numpy import *
+from fonction_py.tools import *
+from fonction_py.preprocess import *
+from sklearn import linear_model
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import cross_validation
+from sklearn.linear_model import LogisticRegression
+from sklearn import tree
+from sklearn import svm
+from sklearn import decomposition
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.grid_search import GridSearchCV
+from sklearn.grid_search import RandomizedSearchCV
+from scipy.stats import uniform as sp_randint
+from sklearn import datasets
+from sklearn.linear_model import Ridge
+from fonction_py.tim import *
 
-def train(xTrain, yTrain):
-    #W = dot(inv(dot(xTrain.T,xTrain)),dot(xTrain.T,yTrain))
-    return 1#(W)
+import time
+
+def faireTout():
+    fields = ['DATE', 'DAY_OFF', 'WEEK_END', 'DAY_WE_DS', 'ASS_ASSIGNMENT', 'CSPL_RECEIVED_CALLS' ] # selectionne les colonnes à lire
+    c = pd.DataFrame()
+    listmodel = faireListModel()
+    #'Evenements',  'Gestion Amex'
+    #setFields = set(pd.read_csv("data/fields.txt", sep=";")['0'].values)
+#    resultat = pd.read_csv("data/submission.txt", sep="\t")
+    
+    i=0
+#    res = []
+    start_time = time.time()
+    model = listmodel[24]
+    data=pd.read_csv("data/trainPure.csv", sep=";", usecols=fields) # LECTURE
+    
+    resultat = pd.read_csv("data/submission.txt", sep="\t") # LECTURE
+    res=[]
+    for model in listmodel:
+        i = i+1
+        print(model[0])
+        x,y = preprocess(data.copy(), model[0]) # rajoute les features
+        model[1].fit(x, y)
+        #model.score(xTrain, yTrain)
+        (xTest, souvenir)=preprocessFINAL(x,model[0])
+        pred = model[1].predict(xTest)
+        pred[pred>max(y)*1.05]=max(y)*1.05
+        pred[pred<0]=0
+        pred =np.round(pred)
+        souvenir['prediction']=int(pred)
+        resultat=pd.merge(resultat, souvenir, how='left',on=['DATE', 'ASS_ASSIGNMENT'])
+        resultat=resultat.fillna(0)
+        resultat['prediction'] = resultat['prediction_x']+resultat['prediction_y']
+        del resultat['prediction_x']
+        del resultat['prediction_y']
+    x,y = preprocess(data.copy(), 'Téléphonie') # rajoute les features
+    #model.score(xTrain, yTrain)
+    (xTest, souvenir)=preprocessFINAL(x,'Téléphonie')
+    pred=telephoniePred(x,y,xTest)
+    pred[pred>max(y)*1.05]=max(y)*1.05
+    pred[pred<0]=0
+    pred =np.round(pred)
+    souvenir['prediction']=int(pred)
+    resultat=pd.merge(resultat, souvenir, how='left',on=['DATE', 'ASS_ASSIGNMENT'])
+    resultat=resultat.fillna(0)
+    resultat['prediction'] = resultat['prediction_x']+resultat['prediction_y']
+    del resultat['prediction_x']
+    del resultat['prediction_y']
+    pd.DataFrame(res).to_csv("reslist.csv", sep=";", decimal=",")
+    resultat.to_csv("vraipred.txt", sep="\t", index =False)    
+    return resultat
+    
+    
+def faireListModel():
+    return [('CAT',  linear_model.LinearRegression()), 
+    ('CMS', RandomForestRegressor(bootstrap=False, criterion='mse', max_depth=5,
+           max_features=30, max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=10, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Crises',linear_model.LinearRegression()),
+    ('Domicile', RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=30,
+           max_features=30, max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=90, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Gestion',RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=30,
+           max_features='auto', max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=100, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Gestion - Accueil Telephonique',RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=20,
+           max_features=30, max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=70, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Gestion Assurances',RandomForestRegressor(bootstrap=False, criterion='mse', max_depth=20,
+           max_features=30, max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=20, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Gestion Clients', RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=10,
+           max_features=90, max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=50, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Gestion DZ', RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=5,
+           max_features=30, max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=30, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Gestion Relation Clienteles',RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=10,
+           max_features=90, max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=110, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Gestion Renault', RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=30,
+           max_features=50, max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=30, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Japon',RandomForestRegressor(bootstrap=False, criterion='mse', max_depth=10,
+           max_features=30, max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=30, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Manager',RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=10,
+           max_features=30, max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=30, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Mécanicien',RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=20,
+           max_features='auto', max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=100, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Médical',RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=30,
+           max_features='auto', max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=100, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Nuit', RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=20,
+           max_features='auto', max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=100, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Prestataires',RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=20,
+           max_features='auto', max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=100, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('RENAULT',RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=80,
+           max_features='auto', max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=100, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('RTC',RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=20,
+           max_features='auto', max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=100, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Regulation Medicale',linear_model.LinearRegression()), 
+    ('SAP',RandomForestRegressor(bootstrap=False, criterion='mse', max_depth=20,
+           max_features=30, max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=30, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Services',RandomForestRegressor(bootstrap=False, criterion='mse', max_depth=30,
+           max_features=30, max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=30, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Tech. Axa',RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=20,
+           max_features='auto', max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=100, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)), 
+    ('Tech. Inter',RandomForestRegressor(bootstrap=False, criterion='mse', max_depth=30,
+           max_features=30, max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=30, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False)),
+    ('Tech. Total',RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=70,
+           max_features='auto', max_leaf_nodes=None, min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           n_estimators=100, n_jobs=1, oob_score=False, random_state=None,
+           verbose=0, warm_start=False))]
